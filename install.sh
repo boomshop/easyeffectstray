3f3c52e4ccb91591ab36ff36eb5a2d337e111145
Markus Schmidt <schmidt@boomshop.net>
2019-12-05 22:12:19 +0100

    Install script and .desktop file

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp pulseeffectstray /usr/local/bin
+chmod a+x /usr/local/bin/pulseeffectstray
+cp pulseeffectstray.png /usr/share/pixmaps
+cp pulseeffectstray.desktop /usr/share/applications
